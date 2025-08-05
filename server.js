e087db3a2fda6fbe284a16d13f6d1e541e73d399
aviisharma238 <aviisharma238@gmail.com>
2025-08-06 00:13:30 +0530

    Your update message

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors";
+
+
 import dotenv from "dotenv";
 import contactRoutes from "./routes/contactRoutes.js";
 
 dotenv.config();
 const app = express();
-const cors = require("cors");
+const cors = require("cors");  
 app.use(cors());
 app.use(express.json());
 
